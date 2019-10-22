847f30f9ab8b9343b9eee06926b58dddffe818c0
David Hirtz <david.hirtz@me.com>
2019-10-22 12:09:28 +0200

    CKEditor buttons

--- assets/ckeditor-bootstrap/skin.js
+++ assets/ckeditor-bootstrap/skin.js
@@ -1,1 +1,10 @@
 ﻿CKEDITOR.skin.name = 'skeleton';
+CKEDITOR.skin.icons = {};
+
+var icons = ['blockquote', 'bold', 'bulletedlist', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'italic', 'link', 'linkbutton', 'numberedlist', 'removeformat' +
+    '', 'source', 'table', 'underline', 'unlink'],
+    path = CKEDITOR.skin.path() + 'icons/';
+
+for (var i = 0; i < icons.length; i++) {
+    CKEDITOR.skin.addIcon(icons[i], path + icons[i] + '.svg');
+}
