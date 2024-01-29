ac8d2aabcd33bd0522e8ed6a740b923dcc6004f2
davidhirtz <david.hirtz@me.com>
2024-01-29 18:25:29 +0100

    Update test.php

--- tests/config/test.php
+++ tests/config/test.php
@@ -1,10 +1,12 @@
 <?php
 
 use davidhirtz\yii2\anakin\Bootstrap;
-use yii\web\Session;
+
+if (is_file(__DIR__ . '/db.php')) {
+    require(__DIR__ . '/db.php');
+}
 
 return [
-    'id' => 'yii2-anakin',
     'aliases' => [
         // This is a fix for the broken aliasing of `BaseMigrateController::getNamespacePath()`
         '@davidhirtz/yii2/anakin' => __DIR__ . '/../../src/',
@@ -13,22 +15,11 @@ return [
         Bootstrap::class,
     ],
     'components' => [
-        'assetManager' => [
-            'basePath' => '@runtime/assets',
-            'linkAssets' => true,
-        ],
         'db' => [
             'dsn' => getenv('MYSQL_DSN') ?: 'mysql:host=127.0.0.1;dbname=yii2_anakin_test',
             'username' => getenv('MYSQL_USER') ?: 'root',
             'password' => getenv('MYSQL_PASSWORD') ?: '',
             'charset' => 'utf8',
-            ...is_file(__DIR__ . '/db.php') ? require (__DIR__ . '/db.php') : [],
-        ],
-        'mailer' => [
-            'useFileTransport' => true,
-        ],
-        'session' => [
-            'class' => Session::class,
         ],
     ],
     'params' => [
