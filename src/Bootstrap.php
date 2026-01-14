853953b1c6210556a09b9f04611eb02c035d88ae
davidhirtz <david.hirtz@me.com>
2026-01-14 17:27:58 +0100

    Update Bootstrap.php

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Hirtz\Anakin;
 
-use Hirtz\Anakin\assets\AnakinAssetBundle;
+use Hirtz\Anakin\Assets\AnakinAssetBundle;
 use Hirtz\Skeleton\Assets\AdminAssetBundle;
-use Hirtz\Skeleton\Assets\TinyMceSkinAssetBundle;
+use Hirtz\Skeleton\Assets\TinyMceAssetBundle;
 use Hirtz\Skeleton\Modules\Admin\Module;
 use Hirtz\Skeleton\Web\Application;
 use Yii;
@@ -15,9 +15,6 @@ use yii\base\Event;
 use yii\i18n\PhpMessageSource;
 use yii\web\View;
 
-/**
- * Overrides the backend theme with the Anakin theme and sets default email contact.
- */
 class Bootstrap implements BootstrapInterface
 {
     /**
@@ -25,55 +22,54 @@ class Bootstrap implements BootstrapInterface
      */
     public function bootstrap($app): void
     {
-        //        Yii::setAlias('@anakin', __DIR__);
-        //        Yii::setAlias('@skeleton/mail/layouts/html', '@anakin/views/layouts/mail');
-        //
-        //        Yii::$app->params['email'] ??= 'hello@anakin.co';
-        //
-        //        Yii::$app->extendComponents([
-        //            'assetManager' => [
-        //                'bundles' => [
-        //                    TinyMceSkinAssetBundle::class => [
-        //                        'sourcePath' => '@anakin/assets/tinymce/skins/',
-        //                    ],
-        //                    AdminAssetBundle::class => [
-        //                        'css' => [], // Remove admin CSS, the Anakin theme will register its own CSS
-        //                        'faviconOptions' => [
-        //                            'href' => '/images/favicons/favicon-32x32.png',
-        //                            'sizes' => '32x32',
-        //                            'type' => 'image/png',
-        //                        ],
-        //                    ],
-        //                ],
-        //            ],
-        //            'i18n' => [
-        //                'translations' => [
-        //                    'anakin' => [
-        //                        'class' => PhpMessageSource::class,
-        //                        'basePath' => '@anakin/../messages',
-        //                    ],
-        //                ],
-        //            ],
-        //            'view' => [
-        //                'theme' => [
-        //                    'pathMap' => [
-        //                        '@skeleton/../resources/views/admin/views/dashboard' => '@anakin/../resources/views/dashboard',
-        //                    ],
-        //                ],
-        //            ]
-        //        ]);
-        //
-        //        // Todo
-        //        //        Event::on(TinyMceEditor::class, TinyMceEditor::EVENT_INIT, function ($event) {
-        //        //            $asset = AnakinAsset::register(Yii::$app->getView());
-        //        //            $event->sender->clientOptions['content_css'] ??= "$asset->baseUrl/css/tinymce.min.css";
-        //        //        });
-        //
-        //        Event::on(View::class, View::EVENT_BEGIN_PAGE, function (): void {
-        //            // @phpstan-ignore-next-line the controller might not be available during an exception in request handling
-        //            if (Yii::$app->controller?->module instanceof Module || Yii::$app->controller?->module->module instanceof Module) {
-        //                AnakinAsset::register(Yii::$app->getView());
-        //            }
+        Yii::setAlias('@anakin', __DIR__);
+        Yii::setAlias('@skeleton/mail/layouts/html', '@anakin/views/layouts/mail');
+
+        Yii::$app->params['email'] ??= 'hello@anakin.co';
+
+        Yii::$app->extendComponents([
+            'assetManager' => [
+                'bundles' => [
+                    TinyMceAssetBundle::class => [
+                        'sourcePath' => '@anakin/../resources/assets/tinymce/skins/',
+                    ],
+                    AdminAssetBundle::class => [
+                        'css' => [], // Remove admin CSS, the Anakin theme will register its own CSS
+                        'faviconOptions' => [
+                            'href' => '/images/favicons/favicon-32x32.png',
+                            'sizes' => '32x32',
+                            'type' => 'image/png',
+                        ],
+                    ],
+                ],
+            ],
+            'i18n' => [
+                'translations' => [
+                    'anakin' => [
+                        'class' => PhpMessageSource::class,
+                        'basePath' => '@anakin/../messages',
+                    ],
+                ],
+            ],
+            'view' => [
+                'theme' => [
+                    'pathMap' => [
+                        '@skeleton/../resources/views/admin/views/dashboard' => '@anakin/../resources/views/dashboard',
+                    ],
+                ],
+            ]
+        ]);
+
+        // Todo
+        //        Event::on(TinyMceEditor::class, TinyMceEditor::EVENT_INIT, function ($event) {
+        //            $asset = AnakinAsset::register(Yii::$app->getView());
+        //            $event->sender->clientOptions['content_css'] ??= "$asset->baseUrl/css/tinymce.min.css";
         //        });
+
+        Event::on(View::class, View::EVENT_BEGIN_PAGE, function (): void {
+            if (Yii::$app->controller?->module instanceof Module || Yii::$app->controller?->module->module instanceof Module) {
+                AnakinAssetBundle::register(Yii::$app->getView());
+            }
+        });
     }
 }
